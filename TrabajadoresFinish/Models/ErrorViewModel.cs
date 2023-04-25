03d7bc2811911c2ec336798913865a78632eab0f
burnex <brunocse123@outlook.com>
2023-04-24 19:17:38 -0500

    Inicio Proyecto

---
+++ TrabajadoresFinish/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TrabajadoresFinish.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
